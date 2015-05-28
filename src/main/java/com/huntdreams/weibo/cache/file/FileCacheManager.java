e35c3490e2f253439cb764c9862da85262bb0a0f
noprom <tyee.noprom@qq.com>
2015-05-28 22:29:44 +0800

    copyCacheTo done
    /**
    * 复制缓存文件
    * @param type
    * @param name
    * @param dist
    * @return 目标文件绝对路径
    * @throws IOException
    */

--- src/main/java/com/huntdreams/weibo/cache/file/FileCacheManager.java
+++ src/main/java/com/huntdreams/weibo/cache/file/FileCacheManager.java
@@ -3,6 +3,7 @@ package com.huntdreams.weibo.cache.file;
 import android.content.Context;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -34,6 +35,13 @@ public class FileCacheManager {
         return mInstance;
     }
 
+    /**
+     * 创建缓存文件
+     * @param type
+     * @param name
+     * @param datas
+     * @throws IOException
+     */
     public void createCache(String type,String name,byte[] datas) throws IOException{
         String path = mCacheDir.getPath() + "/" + type + "/" + name;
         File f = new File(path);
@@ -48,5 +56,38 @@ public class FileCacheManager {
         fos.close();
     }
 
+    /**
+     * 复制缓存文件
+     * @param type
+     * @param name
+     * @param dist
+     * @return 目标文件绝对路径
+     * @throws IOException
+     */
+    public String copyCacheTo(String type,String name, String dist) throws IOException{
+        String path = mCacheDir.getPath() + "/" + type + "/" + name;
+        try{
+            new File(dist).mkdirs();
+        }catch (Exception e){
+            Runtime.getRuntime().exec("mkdir -p " + dist);
+        }
+
+        File origFile = new File(path);
+        File distFile = new File(dist + "/" + name);
+        if(distFile.createNewFile()){
+            FileInputStream fis = new FileInputStream(origFile);
+            FileOutputStream fos = new FileOutputStream(distFile);
+
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while((len = fis.read(buf))!= -1){
+                fos.write(buf,0,len);
+            }
+
+            fis.close();
+            fos.close();
+        }
+        return distFile.getAbsolutePath();
+    }
 
 }
