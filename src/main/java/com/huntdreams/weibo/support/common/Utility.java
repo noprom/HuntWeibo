1a6de80e56d3f23827261dd5697a39bb3f2ae5dc
noprom <tyee.noprom@qq.com>
2015-05-29 23:27:13 +0800

    startServiceAlarm done
    startServiceAlarm done

--- src/main/java/com/huntdreams/weibo/support/common/Utility.java
+++ src/main/java/com/huntdreams/weibo/support/common/Utility.java
@@ -1,6 +1,9 @@
 package com.huntdreams.weibo.support.common;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,6 +16,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class Utility {
 
+    private static final String TAG = Utility.class.getSimpleName();
+
+    private static final int REQUEST_CODE = 100001;
+
+
+
     /**
      * 缓存是否有效
      * @param createTime
@@ -23,7 +32,51 @@ public class Utility {
         return System.currentTimeMillis() <= createTime + TimeUnit.DAYS.toMillis(availableDays);
     }
 
-    public static void startService(Context context){
+    /**
+     * 开启闹钟服务
+     * @param context
+     * @param service
+     * @param intveral
+     */
+    public static void startServiceAlarm(Context context, Class<?> service, long intveral){
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context,service);
+        PendingIntent p = PendingIntent.getService(context, REQUEST_CODE, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME, 0, intveral, p);
+    }
 
+    /**
+     * 开启一个service
+     * @param context
+     */
+    public static void startServices(Context context){
+        Settings settings = Settings.getInstance(context);
+        int interval = getIntervalTime(settings.getInt(Settings.NOTIFICATION_INTERVAL, 1));
+        if(interval > -1){
+
+        }
+    }
+
+    /**
+     * 根据ID获得间隔间隔触发时间
+     * @param id
+     * @return
+     */
+    public static int getIntervalTime(int id) {
+        switch (id){
+            case 0:
+                return 1 * 60 * 1000;
+            case 1:
+                return 3 * 60 * 1000;
+            case 2:
+                return 5 * 60 * 1000;
+            case 3:
+                return 10 * 60 * 1000;
+            case 4:
+                return 30 * 60 * 1000;
+            case 5:
+                return -1;
+        }
+        return -1;
     }
 }
