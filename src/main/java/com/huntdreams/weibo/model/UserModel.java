c555396fd11213f2d7c30319d006e632b8bc6bac
noprom <tyee.noprom@qq.com>
2015-05-30 22:53:31 +0800

    UserModel done
    UserModel done

--- src/main/java/com/huntdreams/weibo/model/UserModel.java
+++ src/main/java/com/huntdreams/weibo/model/UserModel.java
@@ -30,24 +30,24 @@ public class UserModel implements Parcelable {
     public String profile_image_url;
     public String domain;
     public String gender;
-    public String cover_image = "";
-    public String cover_image_phone = "";
-    public String created_at;
-    public String avatar_large;
-    public String verified_reason;
     public int followers_count = 0;
     public int friends_count = 0;
     public int statuses_count = 0;
     public int favourites_count = 0;
     public int verified_type = 0;
+    public String created_at;
     public boolean following = false;
     public boolean allow_all_act_msg = false;
     public boolean geo_enabled = false;
     public boolean verified = false;
     public boolean allow_all_comment = false;
+    public String avatar_large;
+    public String verified_reason;
     public boolean follow_me = false;
     public int online_status = 0;
     public int bi_followers_count = 0;
+    public String cover_image = "";
+    public String cover_image_phone = "";
 
     public String getName() {
         if (TextUtils.isEmpty(remark)){
@@ -99,4 +99,52 @@ public class UserModel implements Parcelable {
         dest.writeString(cover_image);
         dest.writeBooleanArray(new boolean[]{following, allow_all_act_msg, geo_enabled, verified, allow_all_comment});
     }
+
+    public static final Parcelable.Creator<UserModel> CREATOR = new Parcelable.Creator<UserModel>() {
+
+        @Override
+        public UserModel createFromParcel(Parcel input) {
+            UserModel ret = new UserModel();
+            ret.id = input.readString();
+            ret.screen_name = input.readString();
+            ret.name = input.readString();
+            ret.remark = input.readString();
+            ret.province = input.readString();
+            ret.city = input.readString();
+            ret.location = input.readString();
+            ret.description = input.readString();
+            ret.url = input.readString();
+            ret.profile_image_url = input.readString();
+            ret.domain = input.readString();
+            ret.gender = input.readString();
+            ret.created_at = input.readString();
+            ret.avatar_large = input.readString();
+            ret.verified_reason = input.readString();
+            ret.followers_count = input.readInt();
+            ret.friends_count = input.readInt();
+            ret.statuses_count = input.readInt();
+            ret.favourites_count = input.readInt();
+            ret.verified_type = input.readInt();
+            ret.online_status = input.readInt();
+            ret.bi_followers_count = input.readInt();
+            ret.cover_image_phone = input.readString();
+            ret.cover_image = input.readString();
+
+            boolean[] array = new boolean[5];
+            input.readBooleanArray(array);
+
+            ret.following = array[0];
+            ret.allow_all_act_msg = array[1];
+            ret.geo_enabled = array[2];
+            ret.verified = array[3];
+            ret.allow_all_comment = array[4];
+
+            return ret;
+        }
+
+        @Override
+        public UserModel[] newArray(int size) {
+            return new UserModel[size];
+        }
+    };
 }
