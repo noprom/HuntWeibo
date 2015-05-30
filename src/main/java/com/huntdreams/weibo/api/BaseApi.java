d40eac5c230b9a0c32b48f23447f8c9ea775b075
noprom <tyee.noprom@qq.com>
2015-05-30 17:10:44 +0800

    WeiboParameters done
    WeiboParameters done

---
+++ src/main/java/com/huntdreams/weibo/api/BaseApi.java
@@ -0,0 +1,14 @@
+package com.huntdreams.weibo.api;
+
+/**
+ * 微博API基类
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/5/30.
+ */
+public class BaseApi {
+
+    private static final String TAG = BaseApi.class.getSimpleName();
+
+}
---
+++ src/main/java/com/huntdreams/weibo/support/http/HttpUtility.java
@@ -0,0 +1,17 @@
+package com.huntdreams.weibo.support.http;
+
+/**
+ * 网络访问工具类
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/5/30.
+ */
+public class HttpUtility {
+
+    private static final String TAG = HttpUtility.class.getSimpleName();
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+
+    public static String doRequest(String url, )
+}
