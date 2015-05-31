9b8d380f59206d8391a5ebd17dc74ce54fb42035
noprom <tyee.noprom@qq.com>
2015-05-31 16:17:09 +0800

    LoginTask done
    LoginTask done

--- src/main/java/com/huntdreams/weibo/ui/login/LoginActivity.java
+++ src/main/java/com/huntdreams/weibo/ui/login/LoginActivity.java
@@ -1,7 +1,13 @@
 package com.huntdreams.weibo.ui.login;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
@@ -9,10 +15,13 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.huntdreams.weibo.R;
+import com.huntdreams.weibo.api.BaseApi;
+import com.huntdreams.weibo.api.PrivateKey;
 import com.huntdreams.weibo.cache.login.LoginApiCache;
 import com.huntdreams.weibo.support.common.Utility;
 import com.huntdreams.weibo.ui.common.AbsActivity;
 
+import static com.huntdreams.weibo.BuildConfig.DEBUG;
 /**
  * 微博登陆界面
  *
@@ -25,7 +34,7 @@ public class LoginActivity extends AbsActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
     private WebView mWeb;
     private LoginApiCache mLogin;
-    private boolean mIsMutli = false;
+    private boolean mIsMulti = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,7 @@ public class LoginActivity extends AbsActivity {
         super.onCreate(savedInstanceState);
 
         // Init
-        mIsMutli = getIntent().getBooleanExtra("multi", false);
+        mIsMulti = getIntent().getBooleanExtra("multi", false);
         mWeb = Utility.findViewById(this, R.id.login_web);
         mLogin = new LoginApiCache(this);
 
@@ -76,12 +85,112 @@ public class LoginActivity extends AbsActivity {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            return super.shouldOverrideUrlLoading(view, url);
+            if(PrivateKey.isUrlRedirected(url)){
+                view.stopLoading();
+                handleRedirectedUrl(url);
+            }else{
+                view.loadUrl(url);
+            }
+            return true;
         }
 
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            if (!url.equals("about:blank") && PrivateKey.isUrlRedirected(url)) {
+                view.stopLoading();
+                handleRedirectedUrl(url);
+                return;
+            }
             super.onPageStarted(view, url, favicon);
+        }
+    }
+
+    private void handleRedirectedUrl(String url){
+        if (!url.contains("error")) {
+            int tokenIndex = url.indexOf("access_token=");
+            int expiresIndex = url.indexOf("expires_in=");
+            String token = url.substring(tokenIndex + 13, url.indexOf("&", tokenIndex));
+            String expiresIn = url.substring(expiresIndex + 11, url.indexOf("&", expiresIndex));
+
+            if (DEBUG) {
+                Log.d(TAG, "url = " + url);
+                Log.d(TAG, "token = " + token);
+                Log.d(TAG, "expires_in = " + expiresIn);
+            }
+
+            new LoginTask().execute(token, expiresIn);
+        } else {
+            showLoginFail();
+        }
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, Long>{
+
+        private ProgressDialog mProgressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(LoginActivity.this);
+            mProgressDialog.setMessage(getResources().getString(R.string.plz_wait));
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Long doInBackground(String... params) {
+            if(DEBUG){
+                Log.d(TAG, "doInBackground...");
+            }
+            if(!mIsMulti){
+                mLogin.login(params[0], params[1]);
+                return mLogin.getExpireDate();
+            }else{
+                return mLogin.addUser(params[0], params[1]);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Long result) {
+            super.onPostExecute(result);
+            mProgressDialog.dismiss();
+
+            if (!mIsMulti && mLogin.getAccessToken() != null) {
+                if (DEBUG) {
+                    Log.d(TAG, "Access Token:" + mLogin.getAccessToken());
+                    Log.d(TAG, "Expires in:" + mLogin.getExpireDate());
+                }
+                mLogin.cache();
+                BaseApi.setAccessToken(mLogin.getAccessToken());
+            } else if (!mIsMulti && mLogin.getAccessToken() == null) {
+                showLoginFail();
+                return;
+            }
+
+            // Expire date
+            String msg = String.format(getResources().getString(R.string.expires_in), Utility.expireTimeInDays(result));
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            if (!mIsMulti) {
+                                Intent i = new Intent();
+                                i.setAction(Intent.ACTION_MAIN);
+                                i.setClass(LoginActivity.this, MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                setResult(RESULT_OK);
+                                finish();
+                            }
+                        }
+                    })
+                    .create()
+                    .show();
+
         }
     }
 }
