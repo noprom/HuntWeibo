79aa889cdd2274d5acd52459b1569474638a25a4
noprom <tyee.noprom@qq.com>
2015-05-31 12:25:09 +0800

    add some essential pictures
    add some essential pictures

--- src/main/java/com/huntdreams/weibo/ui/common/AbsActivity.java
+++ src/main/java/com/huntdreams/weibo/ui/common/AbsActivity.java
@@ -2,9 +2,14 @@ package com.huntdreams.weibo.ui.common;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.huntdreams.weibo.R;
 import com.huntdreams.weibo.support.common.Settings;
 import com.huntdreams.weibo.support.common.ShakeDetector;
 import com.huntdreams.weibo.support.common.Utility;
@@ -35,10 +40,31 @@ public class AbsActivity extends ToolbarActivity implements ShakeDetector.ShakeL
 
     }
 
+    protected View getSwipeView(){
+        return ((ViewGroup) getWindow().getDecorView()).getChildAt(0);
+    }
+
     @TargetApi(21)
     private void swipeInit(){
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        // TODO init SlidingPaneLayout
+        SlidingPaneLayout v = (SlidingPaneLayout) inflater.inflate(R.layout.swipe_decor_wrapper, null);
+        final ViewGroup frame = (ViewGroup) v.findViewById(R.id.swipe_container);
+        View swipeView = getSwipeView();
+        ViewGroup decor = (ViewGroup) swipeView.getParent();
+        ViewGroup.LayoutParams params = swipeView.getLayoutParams();
+        decor.removeView(swipeView);
+        frame.addView(swipeView);
+        decor.addView(v, params);
+        decor.setBackgroundColor(0);
+
+        // Elevation
+        if(Build.VERSION.SDK_INT >= 21){
+            frame.setElevation(11.8f);
+        } else {
+            v.setShadowResource(R.drawable.panel_shadow);
+        }
+
+
 
     }
 }
