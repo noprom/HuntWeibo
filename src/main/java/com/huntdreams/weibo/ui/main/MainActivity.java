bf5a558bff654b0f0f63feffb7348fe5ff275b2f
noprom <tyee.noprom@qq.com>
2015-05-31 21:55:12 +0800

    MainActivity loaded!
    MainActivity loaded!

--- src/main/java/com/huntdreams/weibo/ui/main/MainActivity.java
+++ src/main/java/com/huntdreams/weibo/ui/main/MainActivity.java
@@ -1,17 +1,15 @@
 package com.huntdreams.weibo.ui.main;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.huntdreams.weibo.R;
-import com.huntdreams.weibo.ui.common.AbsActivity;
 
 import static com.huntdreams.weibo.BuildConfig.DEBUG;
 
 
-public class MainActivity extends AbsActivity {
+public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -23,28 +21,5 @@ public class MainActivity extends AbsActivity {
             Log.d(TAG, "onCreate()");
         }
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
