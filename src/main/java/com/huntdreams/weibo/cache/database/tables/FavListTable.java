86c64ab810db3234fbcc3b57e6ade4d3bec57617
noprom <tyee.noprom@qq.com>
2015-05-31 19:28:55 +0800

    FavListTable done
    FavListTable done

---
+++ src/main/java/com/huntdreams/weibo/cache/database/tables/FavListTable.java
@@ -0,0 +1,23 @@
+package com.huntdreams.weibo.cache.database.tables;
+
+/**
+ *
+ * @author noprom (https://github.com/noprom)
+ * @version 1.0
+ * Created by noprom on 2015/5/31.
+ */
+public class FavListTable
+{
+	public static final String NAME = "fav_list";
+
+	public static final String ID = "id";
+
+	public static final String JSON = "json";
+
+	public static final String CREATE = "create table " + NAME
+						+ "("
+						+ ID + " integer primary key autoincrement,"
+						+ JSON + " text"
+						+ ");";
+}
+
