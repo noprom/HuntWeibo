8917cb67380b52d39f057b48f50790f457e5380f
noprom <tyee.noprom@qq.com>
2015-05-31 20:46:23 +0800

    UserApi done
    UserApi done

--- src/main/java/com/huntdreams/weibo/api/user/UserApi.java
+++ src/main/java/com/huntdreams/weibo/api/user/UserApi.java
@@ -36,4 +36,20 @@ public class UserApi extends BaseApi {
             return null;
         }
     }
+
+    public static UserModel getUserByName(String name){
+        WeiboParameters params = new WeiboParameters();
+        params.put("screen_name", name);
+
+        try {
+            JSONObject json = request(Constants.USER_SHOW, params, HTTP_GET);
+            UserModel user = new Gson().fromJson(json.toString().replaceAll("-Weibo", ""), UserModel.class);
+            return user;
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, "Failed to fetch user info from net: " + e.getClass().getSimpleName());
+            }
+            return null;
+        }
+    }
 }
