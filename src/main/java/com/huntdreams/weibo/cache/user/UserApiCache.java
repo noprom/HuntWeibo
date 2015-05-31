5dc72c995145820d2da3877e1d2afcf3f35969d7
noprom <tyee.noprom@qq.com>
2015-05-31 21:10:13 +0800

    getUser() in UserApiCache done.
    getUser() in UserApiCache done.

--- src/main/java/com/huntdreams/weibo/cache/user/UserApiCache.java
+++ src/main/java/com/huntdreams/weibo/cache/user/UserApiCache.java
@@ -1,17 +1,27 @@
 package com.huntdreams.weibo.cache.user;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
 
+import com.google.gson.Gson;
 import com.huntdreams.weibo.R;
+import com.huntdreams.weibo.api.user.UserApi;
+import com.huntdreams.weibo.cache.Constants;
 import com.huntdreams.weibo.cache.database.DataBaseHelper;
+import com.huntdreams.weibo.cache.database.tables.UsersTable;
 import com.huntdreams.weibo.cache.file.FileCacheManager;
 import com.huntdreams.weibo.model.UserModel;
+import com.huntdreams.weibo.support.common.Utility;
 
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 
+import static com.huntdreams.weibo.BuildConfig.DEBUG;
 /**
  * 用户个人信息缓存
  *
@@ -41,5 +51,38 @@ public class UserApiCache {
 
     public UserModel getUser(String uid){
         UserModel model = UserApi.getUser(uid);
+        if(model == null){
+            Cursor cursor = mHelper.getReadableDatabase().query(UsersTable.NAME, new String[]{
+                    UsersTable.UID,
+                    UsersTable.TIMESTAMP,
+                    UsersTable.USERNAME,
+                    UsersTable.JSON
+            },UsersTable.UID + "?=", new String[]{uid}, null, null, null);
+
+            if(cursor.getCount() >= 1){
+                cursor.moveToFirst();
+                long time = cursor.getLong(cursor.getColumnIndex(UsersTable.TIMESTAMP));
+                if (DEBUG) {
+                    Log.d(TAG, "time = " + time);
+                    Log.d(TAG, "available = " + Utility.isCacheAvailable(time, Constants.DB_CACHE_DAYS));
+                }
+
+                if (Utility.isCacheAvailable(time, Constants.DB_CACHE_DAYS)) {
+                    model = new Gson().fromJson(cursor.getString(cursor.getColumnIndex(UsersTable.JSON)), UserModel.class);
+                    model.timestamp = cursor.getInt(cursor.getColumnIndex(UsersTable.TIMESTAMP));
+                }
+            }
+        }else {
+            // Insert into database
+            ContentValues values = new ContentValues();
+            SQLiteDatabase db = mHelper.getWritableDatabase();
+
+            db.beginTransaction();
+            db.delete(UsersTable.NAME, UsersTable.UID + "=?", new String[]{uid});
+            db.insert(UsersTable.NAME, null, values);
+            db.setTransactionSuccessful();
+            db.endTransaction();
+        }
+        return model;
     }
 }
