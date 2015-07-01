cc205399b8ba25f8e53cfe574447feb742cc5c30
noprom <tyee.noprom@qq.com>
2015-07-01 22:21:30 +0800

    add api Constants

---
+++ app/src/main/java/com/huntdreams/weibo/api/Constants.java
@@ -0,0 +1,15 @@
+package com.huntdreams.weibo.api;
+
+/**
+ * 网络接口地址常量
+ * @author noprom
+ * @email tyee.noprom@qq.com
+ *
+ * Created by noprom on 15/7/1.
+ */
+public class Constants {
+
+    public static final String SINA_BASE_URL = "https://api.weibo.com/2/";
+    // Login
+    public static final String OAUTH2_ACCESS_TOKEN = SINA_BASE_URL + "oauth2/access_token";
+}
