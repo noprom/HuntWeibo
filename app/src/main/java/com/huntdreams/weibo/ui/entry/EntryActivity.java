0c9653f1aeb5af802e37bb951ff98f6299d5a660
noprom <tyee.noprom@qq.com>
2015-07-01 22:18:27 +0800

    init activity

---
+++ app/src/main/java/com/huntdreams/weibo/ui/entry/EntryActivity.java
@@ -0,0 +1,18 @@
+package com.huntdreams.weibo.ui.entry;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+import com.huntdreams.weibo.R;
+
+public class EntryActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+
+
+}
---
+++ app/src/main/java/com/huntdreams/weibo/ui/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.huntdreams.weibo.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.huntdreams.weibo.R;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
