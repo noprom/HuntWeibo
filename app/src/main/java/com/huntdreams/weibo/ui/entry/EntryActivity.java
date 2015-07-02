a469af1de1856739562c8e8f20cfb32a1a715144
noprom <tyee.noprom@qq.com>
2015-07-02 22:57:27 +0800

    EntryActivity done

--- app/src/main/java/com/huntdreams/weibo/ui/entry/EntryActivity.java
+++ app/src/main/java/com/huntdreams/weibo/ui/entry/EntryActivity.java
@@ -1,17 +1,40 @@
 package com.huntdreams.weibo.ui.entry;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.huntdreams.weibo.api.BaseApi;
+import com.huntdreams.weibo.api.login.LoginApiCache;
+import com.huntdreams.weibo.support.Utility;
+import com.huntdreams.weibo.ui.login.LoginActivity;
 
-import com.huntdreams.weibo.R;
-
-public class EntryActivity extends ActionBarActivity {
+public class EntryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entry);
+        LoginApiCache login = new LoginApiCache(this);
+        if(needsLogin(login)){
+            Intent i = new Intent();
+            i.setAction(Intent.ACTION_MAIN);
+            i.setClass(this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }else{
+            BaseApi.setAccessToken(login.getAccessToken());
+            // TODO Enter the main time line
+        }
+    }
+
+    /**
+     * 是否需要登陆
+     * @param login
+     * @return
+     */
+    private boolean needsLogin(LoginApiCache login){
+        // TODO consider expire date
+        return login.getAccessToken() == null || Utility.isTokenExpired(login.getExpireDate());
     }
 
 
