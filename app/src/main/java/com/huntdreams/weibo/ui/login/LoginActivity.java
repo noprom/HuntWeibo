5d092a522bb8a07970590a8486e12b99c44a227c
noprom <tyee.noprom@qq.com>
2015-07-05 23:39:55 +0800

    reload login page

--- app/src/main/java/com/huntdreams/weibo/ui/login/LoginActivity.java
+++ app/src/main/java/com/huntdreams/weibo/ui/login/LoginActivity.java
@@ -7,9 +7,9 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.huntdreams.weibo.R;
 import com.huntdreams.weibo.api.BaseApi;
@@ -22,58 +22,34 @@ public class LoginActivity extends Activity {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
 
-    private TextView mAppId;
-    private TextView mAppSecret;
-    private TextView mUsername;
-    private TextView mPassword;
-
-    private MenuItem mMenuItem;
+    private WebView mWeb;
     private LoginApiCache mLogin;
+    private boolean mIsMulti = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Create login cache instance
+
+        mIsMulti = getIntent().getBooleanExtra("multi", false);
+        // Intialize views
+        mWeb = (WebView) findViewById(R.id.login_web);
+        // Intialize login cache
         mLogin = new LoginApiCache(this);
-        // Init views
-        mAppId = (TextView) findViewById(R.id.app_id);
-        mAppSecret = (TextView) findViewById(R.id.app_secret);
-        mUsername = (TextView) findViewById(R.id.username);
-        mPassword = (TextView) findViewById(R.id.passwd);
+        // Setup login page
+        WebSettings settings = mWeb.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setSaveFormData(false);
+        settings.setSavePassword(false);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
 
-        if(mLogin.getAppId() != null && mLogin.getAppSecret() != null){
-            mAppId.setText(mLogin.getAppId());
-            mAppSecret.setText(mLogin.getAppSecret());
-        }else if(DEBUG){
-            mAppId.setText("211160679");
-//            mAppSecret.setText("63b64d531b98c2dbff2443816f274dd3");
-            mAppSecret.setText("1e6e33db08f9192306c4afa0a61ad56c");
-        }
+        mWeb.setWebViewClient(new LoginWebViewClient());
+        // TODO set PRIVATEKEY
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        mMenuItem = menu.add(R.string.login);
-        mMenuItem.setShowAsAction(1);
-        return true;
-    }
+    private class LoginWebViewClient extends WebViewClient{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item == mMenuItem){
-            new LoginTask().execute(new String[]{
-                    mAppId.getText().toString(),
-                    mAppSecret.getText().toString(),
-                    mUsername.getText().toString(),
-                    mPassword.getText().toString()
-            });
-            return true;
-        }else{
-            return super.onOptionsItemSelected(item);
-        }
     }
-
     private class LoginTask extends AsyncTask<String, Void, Void>{
 
         private ProgressDialog progressDialog;
